0425d602620e1fa085512c6a292200ebb800d3a6
Irvin Ruiz <ruizirvin084@gmail.com>
2022-04-20 13:50:14 -0500

    comepleted user controller refactor to jpa

--- src/main/java/com/example/restblog/data/User.java
+++ src/main/java/com/example/restblog/data/User.java
@@ -2,6 +2,7 @@ package com.example.restblog.data;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Date;
 
@@ -10,14 +11,36 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private Date createdAt;
+    @Column(nullable = false)
     private Role role;
-    private Collection<Post> posts;
+//    private Collection<Post> posts;
 
     public enum Role {USER, ADMIN};
+    @PrePersist
+    void createdAt() {
+        this.createdAt  = new Date();
+    }
+
+    public User(String username, String email, String password, Date createdAt, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.role = Role.USER;
+    }
 }
--- src/main/java/com/example/restblog/web/UsersController.java
+++ src/main/java/com/example/restblog/web/UsersController.java
@@ -1,85 +1,81 @@
 package com.example.restblog.web;
 
-import com.example.restblog.data.Category;
-import com.example.restblog.data.Post;
-import com.example.restblog.data.User;
+import com.example.restblog.data.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
 import java.util.*;
 
-import static com.example.restblog.data.User.Role.USER;
-
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/users", headers = "Accept=application/json")
 public class UsersController {
 
+    private final UsersRepository userRepository;
 
-//    Start of test data
-    static Date date = new Date();
-    static Collection<Post> posts = null;
-    static ArrayList<User> users = new ArrayList<>();
-    static ArrayList<Category> categories = new ArrayList<>();
+    private UsersController(UsersRepository usersRepository) {
+        this.userRepository = usersRepository;
+    }
 
-
-//    Post post1 = new Post(1L, "First post", "Hello this is my first post!", date, new User(), categories);
-//    Post post2 = new Post(2L, "Second post", "Hello this is my second post!", date, new User(), categories);
-//    Post post3 = new Post(3L, "Third post", "Hello this is my third post!", date, new User(), categories);
-    User user1 = new User(1, "username1", "email@1.com", "password", date, USER, posts);
-
-// End of test data
     @GetMapping
     private List<User> getAll() {
-        return users;
+        return userRepository.findAll();
     }
 
     @GetMapping("{id}")
     private User getById(@PathVariable Long id) {
-//        return new User(id, "username" + id, "email@" + id + ".com", "password", date, USER, Arrays.asList(post1, post2, post3));
-        return new User();
+        return userRepository.findById(id).get();
     }
 
     @PostMapping
     private void createUser(@RequestBody User user) {
-        System.out.println(user);
+        User newUser = new User(user.getUsername(), user.getEmail(), user.getPassword(), user.getCreatedAt(), user.getRole());
+        userRepository.save(newUser);
+        System.out.println("User created!");
     }
 
     @PutMapping("{id}")
     private void updateUser(@PathVariable Long id, @RequestBody User user) {
-        System.out.println(id + " updated to " + user);
+        User originalUser = userRepository.findById(id).get();
+        originalUser.setUsername(user.getUsername());
+        originalUser.setEmail(user.getEmail());
+        originalUser.setPassword(user.getPassword());
+
+        userRepository.save(originalUser);
+
     }
 
     @DeleteMapping("{id}")
     private void deleteUser(@PathVariable Long id) {
-        System.out.println("Deleted user with id# " + id);
+        userRepository.delete(userRepository.getById(id));
     }
 
     @GetMapping("/username")
     private User getByUsername(@RequestParam String username) {
-        return new User(1, username, "email@.com", "password", date, USER, posts);
+        return userRepository.findByUsername(username);
     }
 
     @GetMapping("/email")
     private User geyByEmail(@RequestParam String email) {
-        return new User(1, "username", email, "password", date, USER, posts);
+        return userRepository.findByEmail(email);
     }
 
     @GetMapping("{id}/updatePassword")
     private void updatePassword(@PathVariable Long id, @RequestParam(required = false) String oldPassword, @Valid @Size(min = 3) @RequestParam String newPassword) {
-        User user1 = new User(id, "username", "email", "password", date, USER, posts);
-        if (oldPassword.equals(user1.getPassword())) {
+        User originalUser = userRepository.findById(id).get();
+        if (oldPassword.equals(originalUser.getPassword())) {
             System.out.println("Password changed");
-            user1.setPassword(newPassword);
-            System.out.println(user1.getPassword() + " is your new password");
-            System.out.println(user1);
+            originalUser.setPassword(newPassword);
+            System.out.println(originalUser.getPassword() + " is your new password");
+            System.out.println(originalUser);
+            userRepository.save(originalUser);
         } else {
             System.out.println("Wrong Password");
+//        }
         }
-    }
 
 //    todo: add updateEmail
 //    todo: add updateUsername
-
+    }
 }
